f3d5cf6eff09839d6a3518410004c45c79349b59
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-08-05 13:15:33 +0530

    define destructor

--- include/FSWindow.h
+++ include/FSWindow.h
@@ -23,7 +23,7 @@ class BaseWindow {
     public:
     RenderState renderState = {};
     Input input = {};
-    virtual ~BaseWindow();
+    virtual ~BaseWindow(){}
     virtual void swapBuffers() = 0;
     virtual void processMessages() = 0;
     virtual void addConsole() const = 0;
